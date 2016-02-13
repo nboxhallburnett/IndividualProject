11ae358defc03681d674ab74856012baf25e63e0
Nathan Boxhall-Burnett <nathan.bb@live.co.uk>
2016-02-13 02:17:05 +0000

    Added gitignore and base unity project

---
+++ Individual Project Unity/Assets/Scripts/VisibilityManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System;
+
+public abstract class VisibilityManager : MonoBehaviour {
+
+    Camera gameCamera;
+    bool seenInvisible = false;
+    bool seenVisible = false;
+    public Collider objCollider;
+
+    // Use this for initialization
+    void Start () {
+        gameCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        processEffect(SetInvisible, SetVisible);
+    }
+
+    /// <summary>
+    /// Function to call when the object transitions to the 'invisible' state
+    /// </summary>
+    public abstract void SetInvisible();
+
+    /// <summary>
+    /// Function to call when the object transitions to the 'visible' state
+    /// </summary>
+    public abstract void SetVisible();
+
+    bool InCamera () {
+        return GeometryUtility.TestPlanesAABB(GeometryUtility.CalculateFrustumPlanes(gameCamera), objCollider.bounds);
+    }
+
+    bool IsVisible () {
+        return GetComponent<MeshRenderer>().enabled;
+    }
+
+    void processEffect(Action invisibleState, Action visibleState) {
+        if (InCamera() && !seenVisible && IsVisible()) {
+            seenVisible = true;
+        } else if (!InCamera() && seenVisible && IsVisible()) {
+            invisibleState();
+            seenVisible = false;
+        } else if (InCamera() && !seenInvisible && !IsVisible()) {
+            seenInvisible = true;
+        } else if (!InCamera() && seenInvisible && !IsVisible()) {
+            visibleState();
+            seenInvisible = false;
+        }
+    }
+}
