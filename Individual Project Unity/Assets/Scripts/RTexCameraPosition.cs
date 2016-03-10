f0d4c3f841b6ecbd75e2ec6c58c406ccb06734c6
Nathan Boxhall-Burnett <nathan.bb@live.co.uk>
2016-03-10 04:39:13 +0000

    Started work on Camera Perspective script for area link views

---
+++ Individual Project Unity/Assets/Scripts/RTexCameraPosition.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace negeo {
+    [RequireComponent(typeof(Camera))]
+    public class RTexCameraPosition : MonoBehaviour {
+
+        public Transform PointOfView;
+        public Transform RenderPosition;
+        Transform _playerPos;
+        Camera cam;
+
+        // Use this for initialization
+        void Start () {
+            _playerPos = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+            cam = GetComponent<Camera>();
+            cam.aspect = transform.parent.lossyScale.x / transform.parent.lossyScale.y;
+            //cam.fieldOfView = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().fieldOfView;
+        }
+
+        // Update is called once per frame
+        void Update () {
+            Vector3 offset;
+            offset = PointOfView.position - _playerPos.position;
+
+            transform.position = RenderPosition.position - offset;
+            transform.LookAt(RenderPosition);
+            //transform.position = RenderPosition.position;
+            // TODO: Calculate equivelant FOV of the render area relative to the players position, and set this to equal that
+        }
+    }
+}
