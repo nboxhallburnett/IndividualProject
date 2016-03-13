48f6b73b6fae1be373cc51f03cc395aad23f2375
Nathan Boxhall-Burnett <nathan.bb@live.co.uk>
2016-03-13 05:32:21 +0000

    Added player re-positioning when moving 'through' a portal area. added option for additional depth cameras for performance options

---
+++ Individual Project Unity/Assets/NEGeo/Scripts/Helper.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace NEGeo {
+    public static class Helper {
+
+        public static int renderDepth = 5;
+
+        public static T[] FindComponentsInChildrenWithTag<T> (this GameObject parent, string tag) where T : Component {
+            T[] results = new T[0];
+            foreach (T t in parent.GetComponentsInChildren<T>()) {
+                if (t.tag == tag) {
+                    System.Array.Resize(ref results, results.Length + 1);
+                    results[results.Length - 1] = t;
+                }
+            }
+            return results;
+        }
+
+        public static T FindComponentInChildrenWithTag<T> (this GameObject parent, string tag) where T : Component {
+            T[] results = new T[0];
+            foreach (T t in parent.GetComponentsInChildren<T>()) {
+                if (t.tag == tag) {
+                    System.Array.Resize(ref results, results.Length + 1);
+                    results[results.Length - 1] = t;
+                }
+            }
+            return results[0];
+        }
+
+        public static Vector3 RotatePointAroundPivot (Vector3 point, Vector3 pivot, Vector3 angle) {
+            Vector3 dir = point - pivot;
+            dir = Quaternion.Euler(angle) * dir;
+            return dir + pivot;
+        }
+
+        public static void SortDistances (ref Vector3[] positions, Vector3 origin) {
+            float[] distances = new float[positions.Length];
+            for (int i = 0; i < positions.Length; i++) {
+                distances[i] = (positions[i] - origin).sqrMagnitude;
+            }
+            System.Array.Sort(distances, positions);
+        }
+    }
+}
--- Individual Project Unity/Assets/NEGeo/Scripts/TransportArea.cs
+++ Individual Project Unity/Assets/NEGeo/Scripts/TransportArea.cs
@@ -4,8 +4,15 @@ using System.Collections;
 namespace NEGeo {
     public class TransportArea : MonoBehaviour {
 
+        RTexCameraPosition perspective;
+
         // Use this for initialization
         void Start () {
+            perspective = GetComponentInChildren<RTexCameraPosition>();
+            if (perspective == null) {
+                Debug.LogError("No RTexCameraPosition script attached to child.");
+                return;
+            }
 
         }
 
@@ -17,6 +24,9 @@ namespace NEGeo {
         void OnTriggerStay (Collider col) {
             // TODO: Some fancy shmancy stuff in here for player re-location. Probably something along
             //       the lines of 'If the player is > half way through this area, move it relative to the linked one
+            if (perspective != null && col.tag == "Player") {
+                perspective.positionPlayer(col, transform.position, transform.forward);
+            }
         }
 
 
